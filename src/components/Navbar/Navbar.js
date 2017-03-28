6cedcc19a4edbc2bc26fba66b318ec950cb3eee7
Viktors Jenovs <jviktors@gmail.com>
2017-03-28 20:25:29 +0300

    Add Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,10 +4,15 @@ import { Link } from 'react-router';
 import './main.scss';
 
 const Navbar = (props) => {
+  const { pathname } = props;
+  const projectName = pathname.substr(1).replace('-', ' ');
   return (
     <nav>
-      <div>{props.pathname}</div>
-      <Link to={'/'}>Home</Link>
+      <div>
+        <Link to={'/'}>Home</Link>
+        <div className="navbar__project-div">{projectName}</div>
+        <div className="navbar__image-div"></div>
+      </div>
     </nav>
   )
 }
