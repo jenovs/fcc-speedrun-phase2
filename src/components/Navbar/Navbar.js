3b033ed7963c65c4199cdb91647e6743fb84d1ec
Viktors Jenovs <jviktors@gmail.com>
2017-03-29 11:00:47 +0300

    Add Medium link to the Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Link } from 'react-router';
 
 import './main.scss';
+import links from './../../medium-links.json';
+
+const openMedium = (pathname) => {
+  window.open(links[pathname])
+}
 
 const Navbar = (props) => {
   const { pathname } = props;
@@ -11,7 +16,10 @@ const Navbar = (props) => {
       <div>
         <Link to={'/'}>Home</Link>
         <div className="navbar__project-div">{projectName}</div>
-        <div className="navbar__image-div"></div>
+        <div
+          onClick={openMedium.bind(null, pathname)}
+          className="navbar__image-div"
+        ></div>
       </div>
     </nav>
   )
