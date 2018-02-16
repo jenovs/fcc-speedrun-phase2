07b00b389276c92d946c924a4fdf1f0273c7576b
Viktors Jenovs <jviktors@gmail.com>
2018-02-16 15:00:26 +0200

    Replace stock-chart api endpoint

--- src/components/RandomQuoteMachine/App.js
+++ src/components/RandomQuoteMachine/App.js
@@ -5,7 +5,6 @@ import Home from './Home';
 import tickers from './nasdaq100';
 
 export default class App extends React.Component {
-
   constructor(props) {
     super(props);
 
@@ -22,40 +21,47 @@ export default class App extends React.Component {
       rand = Math.floor(Math.random() * tickers.length);
     } while (rand === this.state.rand);
 
-    this.setState({
-      rand,
-      name: null,
-      data: null,
-      close: null
-    }, this.fetchData(tickers[rand]));
+    this.setState(
+      {
+        rand,
+        name: null,
+        data: null,
+        close: null,
+      },
+      this.fetchData(tickers[rand])
+    );
   }
 
   fetchData(ind) {
-    fetch(`https://stock-chart.jenovs.com/graph/${ind}`)
-    .then(res => res.json())
-    .then(json => {
-      if (!json.dataset) return this.setRandomTicker();
-      this.setState({
-        name: json.dataset.name.match(/(.+\([A-Z]+\))/)[0],
-        data: json.dataset.data,
-        close: json.dataset.data[0][4]
+    fetch(`https://charts.jenovs.com/graph/${ind}`)
+      .then(res => res.json())
+      .then(json => {
+        if (!json.dataset) return this.setRandomTicker();
+        this.setState({
+          name: json.dataset.name.match(/(.+\([A-Z]+\))/)[0],
+          data: json.dataset.data,
+          close: json.dataset.data[0][4],
+        });
       })
-    })
-    .catch(e => console.log(e));
+      .catch(e => console.log(e));
   }
 
   render() {
     const { name, data, close } = this.state;
     const props = {
-      name, data, close,
-      updateQuote: this.setRandomTicker.bind(this)
-    }
-    const childrenWithProps = React.Children.map(this.props.children, (child) => React.cloneElement(child, props));
+      name,
+      data,
+      close,
+      updateQuote: this.setRandomTicker.bind(this),
+    };
+    const childrenWithProps = React.Children.map(this.props.children, child =>
+      React.cloneElement(child, props)
+    );
 
     return (
       <div className="app__container">
         <Home {...props} />
       </div>
-    )
+    );
   }
 }
